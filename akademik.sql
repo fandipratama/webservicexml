19dec9a56007b35290492e73a81e7a4378b00768
fandipratama <fandi21@mhs.unisbank.ac.id>
2017-03-13 22:18:05 +0700

    Add files via upload

---
+++ akademik.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.7.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 13, 2017 at 04:13 PM
+-- Server version: 5.6.20-log
+-- PHP Version: 5.5.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `akademik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE IF NOT EXISTS `mahasiswa` (
+  `nim` varchar(15) NOT NULL,
+  `nama` varchar(150) NOT NULL,
+  `alamat` varchar(250) NOT NULL,
+  `progdi` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `nama`, `alamat`, `progdi`) VALUES
+('14.01.53.0116', 'Fandi Pratama', 'Pemalang', 'Teknik Informatika'),
+('14.01.53.0109', 'Alfun Adam', 'Tegal', 'Teknik Informatika'),
+('14.01.53.0104', 'Riva Ayu', 'Pati', 'Teknik Informatika');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
